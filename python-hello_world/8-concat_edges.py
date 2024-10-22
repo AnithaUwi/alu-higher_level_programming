dd930069300a38b76370c03eb311f2f8ef708f00
anitha <a.uwimpuhwe@alustudent.com>
2024-10-22 10:05:40 +0200

    done

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
-         language that combines remarkable power with very clear syntax"
+                 language that combines remarkable power with very clear syntax"
 # YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
-print(str[39:66] + str[114:120] + str[:6])
+print(str[39:66] + str[122:128] + str[:6])
