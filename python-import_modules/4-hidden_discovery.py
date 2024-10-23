35c8febcb61b05d7647f58464ebb5a886b33caef
anitha <a.uwimpuhwe@alustudent.com>
2024-10-23 15:07:16 +0200

    done

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -2,14 +2,13 @@
 import hidden_4
 
 if __name__ == "__main__":
-# Get all names defined in the module and filter out names starting with "__"
+    # Get all names defined in the module and filter out names starting with "__"
     names = dir(hidden_4)
     filtered_names = [name for name in names if not name.startswith("__")]
 
-# Sort the names in alphabetical order
+    # Sort the names in alphabetical order
     filtered_names.sort()
 
-# Print each name on a new line
+    # Print each name on a new line
     for name in filtered_names:
         print(name)
-
