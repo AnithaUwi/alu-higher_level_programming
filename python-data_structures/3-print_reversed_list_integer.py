c3c34e4ffb1996f6142212531ba42fdd60372661
anitha <a.uwimpuhwe@alustudent.com>
2024-10-23 17:26:37 +0200

    done

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -2,4 +2,5 @@
 def print_reversed_list_integer(my_list=[]):
     # Iterate over the list in reverse order
     for i in range(len(my_list) - 1, -1, -1):
-        print("{}".format(my_list[i]))  # Print each integer using)
+        print("{:d}".format(my_list[i]))  # Use the correct f
+
