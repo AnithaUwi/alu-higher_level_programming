8f945cacc9927f977724f0644c04ac9ea6559982
anitha <a.uwimpuhwe@alustudent.com>
2024-10-23 14:50:20 +0200

    done

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -4,7 +4,13 @@ import sys
 if __name__ == "__main__":
     total = 0
 
-    for arg in sys.argv[1:]:  # Skip the script name
-        total += int(arg)  # Convert argument to integer and add to total
+ # Check if there are arguments
+    if len(sys.argv) == 1:
+        print(0)  # No arguments passed
+    else:
+     # Sum the arguments, convert to integer
+        for arg in sys.argv[1:]:
+            total += int(arg)  # Convert argument to integer and add to total
 
-        print(total)  # Print the final total
+            print(total)  # Print the total sum
+
