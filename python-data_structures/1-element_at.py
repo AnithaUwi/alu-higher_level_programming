f44b2b50651b5dc30a6bf9507d0b9e52854d1fa1
anitha <a.uwimpuhwe@alustudent.com>
2024-10-25 11:15:11 +0200

    done

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    # Check if the index is negative or out of range
     if idx < 0 or idx >= len(my_list):
         return None
-        return my_list[idx]  # Return the element at the given index
+    return my_list[idx]
