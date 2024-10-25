310d0ff66a702f30180cc95993021af46212e95e
anitha <a.uwimpuhwe@alustudent.com>
2024-10-25 13:07:52 +0200

    done

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -5,5 +5,5 @@ def max_integer(my_list=[]):
     max_value = my_list[0]  # Initialize max_value with t
     for num in my_list:  # Iterate through each numb
             if num > max_value:  # Compare with max_value
-                max_value = num   # Update max_value if                                     
+                max_value = num  # Update max_value if                                     
     return max_value
